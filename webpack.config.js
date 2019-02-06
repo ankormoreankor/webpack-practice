6771d476d948fac3d8888f6c5f9b8ebdcf5cefa7
Grishin Anton <grishin_rostov@mail.ru>
2019-02-06 20:48:33 +0300

    Решение первой домашки

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 // Core
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 const delay = (timeout = 1000) =>
     new Promise(resolve => setTimeout(resolve, timeout));
+
+// the path(s) that should be cleaned
+let pathsToClean = ['dist']
+
+// the clean options to use
+let cleanOptions = {
+    verbose: true,
+}
 
 /**
  * Типы конфигов вебпак:
@@ -12,15 +21,16 @@ const delay = (timeout = 1000) =>
  */
 module.exports = () => {
     return {
-        mode: 'none',
-        devtool: false,
-        plugins: [
-            // Каждый плагин — это конструктор
-            new HtmlWebpackPlugin({
-                template: './static/template.html',
-                title: 'Изучаем вебпак! 🚀',
-                favicon: './static/favicon.ico',
-            }),
-        ],
+      mode: "none",
+      devtool: false,
+      plugins: [
+        // Каждый плагин — это конструктор
+        new HtmlWebpackPlugin({
+          template: "./static/template.html",
+          title: "Изучаем вебпак! 🚀",
+          favicon: "./static/favicon.ico"
+        }),
+        new CleanWebpackPlugin(pathsToClean, cleanOptions)
+      ]
     };
 };
