10d1827d047221b6fdf20632f9e4381ed7e5716d
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-15 19:49:53 +0200

    → Lesson 4. Checkpoint at: 20:59:17

--- scripts/webpack/modules/css.js
+++ scripts/webpack/modules/css.js
@@ -59,18 +59,6 @@ export const loadCss = () => ({
                     },
                 ],
             },
-            // loadImages
-            {
-                test: /\.(png|jpg|jpeg)$/,
-                use:  [
-                    {
-                        loader:  'file-loader',
-                        options: {
-                            name: 'images/[name].[ext]',
-                        },
-                    },
-                ],
-            },
         ],
     },
 });
