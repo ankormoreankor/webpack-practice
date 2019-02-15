382f63eb945b7ce2a3bfa8be0167c24fea27f5bb
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-15 19:49:53 +0200

    → Lesson 4. Checkpoint at: 20:28:28

--- scripts/webpack/config/webpack.common.js
+++ scripts/webpack/config/webpack.common.js
@@ -80,6 +80,17 @@ module.exports = () => {
                         },
                     ],
                 },
+                {
+                    test: /\.(png|jpg|jpeg)$/,
+                    use:  [
+                        {
+                            loader:  'file-loader',
+                            options: {
+                                name: `./images/${CHUNK_NAME_ASSET}`,
+                            },
+                        },
+                    ],
+                },
             ],
         },
         plugins: [
