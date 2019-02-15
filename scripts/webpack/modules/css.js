0baa88e882901bfa7f3ab248387cc8d3a1ffab8e
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-15 19:49:53 +0200

    → Lesson 4. Checkpoint at: 20:56:33

---
+++ scripts/webpack/modules/css.js
@@ -0,0 +1,73 @@
+export const loadCss = () => ({
+    module: {
+        rules: [
+            // loadJavaScript
+            // loadCss
+            {
+                // TODO: прокачать загрузку стилей
+                test: /\.css$/,
+                use:  [
+                    'style-loader',
+                    {
+                        loader:  'css-loader',
+                        options: {
+                            modules:        true,
+                            localIdentName:
+                                '[path][name]__[local]--[hash:base64:5]',
+                        },
+                    },
+                    {
+                        loader:  'postcss-loader',
+                        options: {
+                            plugins: [
+                                // цепочка плагинов postcss
+
+                                env({
+                                    stage:    0, // default: stage 2
+                                    features: {
+                                        'custom-media-queries': {
+                                            importFrom: [
+                                                {
+                                                    customMedia: {
+                                                        '--phonePortrait':
+                                                            '(width <= 414px)',
+                                                        '--phoneLandscape':
+                                                            '(width >= 415px) and (width <= 667px)',
+                                                        '--tabletPortrait':
+                                                            '(width >= 668px) and (width <= 768px)',
+                                                        '--tabletLandscape':
+                                                            '(width >= 769px) and (width <= 1024px)',
+                                                        '--desktopS':
+                                                            '(width >= 1025px) and (width <= 1366px)',
+                                                        '--desktopM':
+                                                            '(width >= 1367px) and (width <= 1680px)',
+                                                        '--desktopL':
+                                                            '(width >= 1681px) and (width <= 1920px)',
+                                                        '--desktopXL':
+                                                            '(width >= 1921px)',
+                                                    },
+                                                },
+                                            ],
+                                        },
+                                    },
+                                }),
+                            ],
+                        },
+                    },
+                ],
+            },
+            // loadImages
+            {
+                test: /\.(png|jpg|jpeg)$/,
+                use:  [
+                    {
+                        loader:  'file-loader',
+                        options: {
+                            name: 'images/[name].[ext]',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+});
