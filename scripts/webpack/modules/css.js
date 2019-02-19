c1b70499c2613f80a9777d54b05eff4b8c25c0b9
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-19 16:04:14 +0200

    → Lesson 5. Checkpoint at: 16:44:32

--- scripts/webpack/modules/css.js
+++ scripts/webpack/modules/css.js
@@ -1,6 +1,47 @@
 // Core
 import env from 'postcss-preset-env';
 
+// cssnano → минификация
+const loadPostcss = (
+    { sourceMap = false, minify = false } = { sourceMap: false, minify: false },
+) => ({
+    loader:  'postcss-loader',
+    options: {
+        sourceMap,
+        plugins: [
+            // цепочка плагинов postcss
+
+            env({
+                stage:    0, // default: stage 2
+                features: {
+                    'custom-media-queries': {
+                        importFrom: [
+                            {
+                                customMedia: {
+                                    '--phonePortrait':  '(width <= 414px)',
+                                    '--phoneLandscape':
+                                        '(width >= 415px) and (width <= 667px)',
+                                    '--tabletPortrait':
+                                        '(width >= 668px) and (width <= 768px)',
+                                    '--tabletLandscape':
+                                        '(width >= 769px) and (width <= 1024px)',
+                                    '--desktopS':
+                                        '(width >= 1025px) and (width <= 1366px)',
+                                    '--desktopM':
+                                        '(width >= 1367px) and (width <= 1680px)',
+                                    '--desktopL':
+                                        '(width >= 1681px) and (width <= 1920px)',
+                                    '--desktopXL': '(width >= 1921px)',
+                                },
+                            },
+                        ],
+                    },
+                },
+            }),
+        ],
+    },
+});
+
 const loadCss = ({ sourceMap = false } = { sourceMap: false }) => ({
     loader:  'css-loader',
     options: {
@@ -18,7 +59,7 @@ export const loadDevCss = () => ({
             // css loader → sourcemaps
             loadCss({ sourceMap: true }),
             // postcss loader
-            // ?
+            loadPostcss({ sourceMap: true }),
         ],
     },
 });
@@ -31,60 +72,23 @@ export const loadProdCss = () => ({
             // css loader → minify
             loadCss({ sourceMap: false }),
             // postcss loader
-            // ?
+            loadPostcss({ sourceMap: false }),
         ],
     },
 });
 
-export const loadCsss = () => ({
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use:  [
-                    {
-                        loader:  'postcss-loader',
-                        options: {
-                            plugins: [
-                                // цепочка плагинов postcss
-
-                                env({
-                                    stage:    0, // default: stage 2
-                                    features: {
-                                        'custom-media-queries': {
-                                            importFrom: [
-                                                {
-                                                    customMedia: {
-                                                        '--phonePortrait':
-                                                            '(width <= 414px)',
-                                                        '--phoneLandscape':
-                                                            '(width >= 415px) and (width <= 667px)',
-                                                        '--tabletPortrait':
-                                                            '(width >= 668px) and (width <= 768px)',
-                                                        '--tabletLandscape':
-                                                            '(width >= 769px) and (width <= 1024px)',
-                                                        '--desktopS':
-                                                            '(width >= 1025px) and (width <= 1366px)',
-                                                        '--desktopM':
-                                                            '(width >= 1367px) and (width <= 1680px)',
-                                                        '--desktopL':
-                                                            '(width >= 1681px) and (width <= 1920px)',
-                                                        '--desktopXL':
-                                                            '(width >= 1921px)',
-                                                    },
-                                                },
-                                            ],
-                                        },
-                                    },
-                                }),
-                            ],
-                        },
-                    },
-                ],
-            },
-        ],
-    },
-});
+// export const loadCsss = () => ({
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.css$/,
+//                 use:  [
+//                     ,
+//                 ],
+//             },
+//         ],
+//     },
+// });
 
 export const loadSass = () => ({
     module: {
